f931c7f1d82526575d1c06cce5e044598e07b883
Devendra Prasad <devendraprasad1984@gmail.com>
2021-09-12 14:13:37 +0530

    update

--- react-ssr/src/components/screens/home.js
+++ react-ssr/src/components/screens/home.js
@@ -1,6 +1,18 @@
-const Home=props=>{
+import {config} from "../../configs/config";
+import NoData from "../common/nodata";
+
+
+const Home = props => {
+    const summaryData = config.localdata.SUMMARY
+    const displaySummary = () => {
+        return summaryData.map((summary, index) => {
+            return <div key={'summary-key-' + index} dangerouslySetInnerHTML={{__html: summary}}/>
+        })
+    }
+    if (summaryData === undefined || summaryData.length === 0) return <NoData text={'please wait...'}/>
     return <div>
         <h1>home</h1>
+        {displaySummary()}
     </div>
 }
 export default Home
--- react-ssr/src/configs/localdata.js
+++ react-ssr/src/configs/localdata.js
@@ -1,7 +1,9 @@
 import linksData from '../resources/links.json'
+import summaryData from '../resources/summary.json'
 
 
 const localdata = {
+    SUMMARY: summaryData,
     LINKS: linksData
 }
 export default localdata

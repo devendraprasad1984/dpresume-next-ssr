f789b0f05780986d5f9fe693349112d52967c85e
Devendra Prasad <devendraprasad1984@gmail.com>
2021-09-12 09:02:51 +0530

    update

--- config/index.js
+++
@@ -1,5 +0,0 @@
-const dev = process.env.NODE_ENV !== 'production'
-//export const server = dev ? 'http://localhost:3000' : 'https://jsonplaceholder.typicode.com/posts'
-export const endpoints = {
-    posts: "https://jsonplaceholder.typicode.com/posts"
-}

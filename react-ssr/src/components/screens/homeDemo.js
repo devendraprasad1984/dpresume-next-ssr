cd997c0ef01989d464606a88b6fc31c5e7ca7241
Devendra Prasad <devendraprasad1984@gmail.com>
2021-09-13 10:06:20 +0530

    update

--- react-ssr/src/components/screens/homeDemo.js
+++ react-ssr/src/components/screens/homeDemo.js
@@ -1,20 +1,24 @@
 import {config} from "../../configs/config";
+import {useState} from "react";
 
 
 const HomeDemo = () => {
+    const [videoMode, setVideoMode] = useState(false)
+
     const demoPageContent = () => {
         let res = config.localdata.HOME_DEMO
         let {links, youtube} = res
-        let printLinks = () => links.map((x,i) => {
+        let printLinks = () => links.map((x, i) => {
             let num = Math.floor(Math.random() * config.colors.length)
             let color = config.colors[num] || 'white'
-            return <a key={'link-demo'+i} className="white pcenter mcard" target="_blank" href="${x.href}">
+            return <a key={'link-demo' + i} className="white pcenter mcard" target="_blank" href="${x.href}">
                 <span style={{color: color}} className="center">{x.name}</span>
             </a>
         })
 
-        let printYouTubeVideos = () => youtube.map((x,i) => {
-            let vtag = <span key={'link-video-demo'+i}>
+        let printYouTubeVideos = () => youtube.map((x, i) => {
+            if (!videoMode) return null
+            let vtag = <span key={'link-video-demo' + i}>
                 <h2>{x.name}</h2>
                 <iframe width="100%" height="80%" src={x.src} frameBorder="0" allowFullScreen="allowfullscreen"/>
                 </span>
@@ -24,8 +28,10 @@ const HomeDemo = () => {
         return <div className='margin-ud'>
             <h1>Demo Examples</h1>
             <div className="flexbox cards">{printLinks()}</div>
-            <br/><br/>
-            <h1>few Video Demo</h1>
+            <br/>
+            <div className='row'>
+                <span className='btn danger' onClick={() => setVideoMode(!videoMode)}>Click To see Video Demo</span>
+            </div>
             <div className="flexbox-video video-cards">{printYouTubeVideos()}</div>
         </div>
     }

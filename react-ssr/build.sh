4bad04255a2093bdb738bbd4891c15ee50f991b0
Devendra Prasad <devendraprasad1984@gmail.com>
2021-09-13 10:26:46 +0530

    update

--- react-ssr/build.sh
+++ react-ssr/build.sh
@@ -1,2 +1,6 @@
+echo "starting build"
+echo "cleaning up older build"
 rm -rf build/
+echo "building..."
 npm run build
+echo "completed"
--- react-ssr/src/components/common/headerInfo.js
+++ react-ssr/src/components/common/headerInfo.js
@@ -4,7 +4,7 @@ import {config} from "../../configs/config";
 const HeaderInfo = props => {
     return <div className='margin-ud'>
         <div className='row'>
-            <h1 className='size30'>{config.name} <a href={config.cvLink} target='_blank' className='bl size30'>{config.chars.down}</a></h1>
+            <h1 className='size30'>{config.name} <a href={config.cvLink} target='_blank' className='bl size30 xred'>{config.chars.down}</a></h1>
             <h2>{config.rightTitle}</h2>
         </div>
         <div className='row xviolet'>

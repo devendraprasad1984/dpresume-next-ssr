1dd643bd649774706a5ab78d5e5a6b1900c6b7c6
Devendra Prasad <devendraprasad1984@gmail.com>
2021-09-13 22:09:27 +0530

    update

--- react-ssr/src/App.test.js
+++ react-ssr/src/App.test.js
@@ -1,13 +1,16 @@
 import React from "react";
 
-import {shallow} from 'enzyme'
+import {mount, shallow} from 'enzyme'
 import App from "./App";
 import Home from "./components/screens/home";
 import OneLinerHeader from "./components/common/oneLinerHeader";
 import Education from "./components/screens/education";
 import Experience from "./components/screens/experience";
+import HomeDemo from "./components/screens/homeDemo";
 
 
+//mount: mounts component DOM including child component
+//shallow: shallow references to component without rendering child components
 describe('testing rendering of components without crashing', () => {
     it('testing app', () => {
         shallow(<App/>)
@@ -30,5 +33,21 @@ describe('testing Home Component', () => {
         const onelineHeader = <h1 className='active size25'>{'hello'}</h1>
         expect(home.contains(oneliner)).toEqual(true)
         expect(oneliner.contains(onelineHeader)).toEqual(true)
+    })
+})
+
+describe('testing basic list component props', () => {
+    const basicList = ["one", "two"]
+    const basicDisplay = mount(<BasicDisplay list={basicList}/>)
+    it('checking basic display accepts prop', () => {
+        expect(basicDisplay.props().list).toEqual(basicList)
+    })
+})
+
+describe('testing home component demo video button click', () => {
+    const homeDemo = mount(<HomeDemo/>)
+    const label = homeDemo.find('#').text()
+    it('video button label & click', () => {
+        expect(label).toEqual('Click To see Video Demo')
     })
 })

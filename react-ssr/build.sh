7fb676bfe70dfdb5081abe4fa4b2d8413f80e33c
Devendra Prasad <devendraprasad1984@gmail.com>
2021-09-13 11:10:03 +0530

    update

--- react-ssr/build.sh
+++ react-ssr/build.sh
@@ -3,4 +3,6 @@ echo "cleaning up older build"
 rm -rf build/
 echo "building..."
 npm run build
-echo "completed"
+echo "deploying to server"
+cp build/* /Volumes/dpresume.com/httpdocs
+echo "success. completed"

e55ed47723a79fd61db04808805fb81621a4613a
Devendra Prasad <devendraprasad1984@gmail.com>
2021-09-16 11:26:10 +0530

    update

--- react-ssr/src/components/screens/notes.js
+++ react-ssr/src/components/screens/notes.js
@@ -2,17 +2,23 @@ import React from "react";
 import OneLinerHeader from "../common/oneLinerHeader";
 import PropTypes from "prop-types";
 
-
 const Notes = (props) => {
-    return (
-        <div>
-            <OneLinerHeader title={props.title}/>
-            <iframe title='my_notes' src="https://drive.google.com/file/d/1rOr4G8if6mjdYkOdUZPorGMgDkvBJu6K/preview" width="640" height="480"
-                    allow="autoplay" width='100%' height='560px'></iframe>
-        </div>
-    );
+  return (
+    <div>
+      <OneLinerHeader title={props.title} />
+      <iframe
+        title="my_notes"
+        src="https://drive.google.com/file/d/1rOr4G8if6mjdYkOdUZPorGMgDkvBJu6K/preview"
+        width="640"
+        height="480"
+        allow="autoplay"
+        width="100%"
+        height="560px"
+      ></iframe>
+    </div>
+  );
 };
 Notes.propTypes = {
-    title: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 };
 export default Notes;

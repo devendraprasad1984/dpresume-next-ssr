4c2e7e0f19aedaf30676018cc5a59b0d01e86d5e
devendra prasad <devendraprasad1984@gmail.com>
2022-01-23 21:05:51 +0530

    updates

--- src/components/screens/welcome.js
+++ src/components/screens/welcome.js
@@ -6,22 +6,22 @@ import PropTypes from "prop-types";
 const Welcome = (props) => {
     const [isWelcome, setIsWelcome] = useState(true)
     const [counter, setCounter] = useState(5)
+    let welcomeTimerRef
 
     const onClose = () => {
         clearInterval(welcomeTimerRef)
         setIsWelcome(false)
         props.onClose()
     }
-    let welcomeTimerRef
 
     const handleTimer = () => {
-        if (welcomeTimerRef !== undefined) return
-        welcomeTimerRef = setInterval(() => {
+        welcomeTimerRef = !welcomeTimerRef && setInterval((p1) => {
             setCounter((oldVal) => {
                 if (oldVal === 0) onClose()
                 return oldVal - 1
             })
-        }, 1000)
+            console.log('updated', p1)
+        }, 1000, counter)
     }
 
     useEffect(() => {
@@ -30,21 +30,23 @@ const Welcome = (props) => {
         handleTimer()
         //unmount
         return () => {
+            clearInterval(welcomeTimerRef)
             setCounter(c => 0)
             setIsWelcome(p => false)
-            clearInterval(welcomeTimerRef)
         }
     }, [])
 
-    return <div className='center size30'>
+    return <div className='center size30 parent-center'>
         <Modalify
             tagid="modalwelcome"
             show={isWelcome}
             close={onClose}
         >
-            <h2>Welcome mate, How are you doing...</h2>
-            <h4>We will go to Home page in </h4>
-            <h1 className='xred size35 center'>{counter}</h1>
+            <div className='child-center'>
+                <h2>Welcome mate, How are you doing...</h2>
+                <h4>We will go to Home page in </h4>
+                <h1 className='xred size35 center'>{counter}</h1>
+            </div>
         </Modalify>
     </div>
 }

9dc7ec6b786f2155dcadfcfef5d0f402cbfd6b35
devendra prasad <devendraprasad1984@gmail.com>
2022-01-24 13:14:36 +0530

    schema validation api results - jsonshcema

--- src/components/screens/home.js
+++ src/components/screens/home.js
@@ -1,4 +1,4 @@
-import PropTypes from "prop-types";
+import PropTypes, {string} from "prop-types";
 import React from "react";
 
 import {config} from "../../configs/config";
@@ -7,22 +7,38 @@ import BasicDisplay from "../common/basicDisplay";
 import NoData from "../common/nodata";
 import OneLinerHeader from "../common/oneLinerHeader";
 import HomeDemo from "./homeDemo";
+import Article from "./articles";
 // import CurrentlyWorkingAt from "./currentlyWorkingAt";
 // import '../../webcomponents/counter'
+import {Validator} from "jsonschema";
+
+const validator = new Validator()
+const articleSchema = {
+    id: '/article-schema',
+    properties: {
+        title: {type: "string"},
+        upvotes: {types: "number"},
+        date: {type: 'string'}
+    },
+    required: ['title', 'upvotes', 'date']
+}
+const article = [
+    {
+        title: "helloF",
+        upvotes: 12,
+        date: "2021-01-02",
+    },
+    {
+        title: "ABC",
+        upvotes: 22,
+        date: "2020-08-10",
+    },
+];
+// validator.addSchema(articleSchema, '/article-schema')
+const isArticlesValid = validator.validate(article, articleSchema).valid
+console.log('article valid', isArticlesValid)
 
 const Home = (props) => {
-    const article = [
-        {
-            title: "helloF",
-            upvotes: 12,
-            date: "2021-01-02",
-        },
-        {
-            title: "ABC",
-            upvotes: 22,
-            date: "2020-08-10",
-        },
-    ];
     const {data, loading, error} = useAPI(config.endpoints.SUMMARY);
     if (loading) return <NoData text={config.messages.PLZ_WAIT}/>;
     if (error) return <NoData text={config.messages.ERROR}/>;

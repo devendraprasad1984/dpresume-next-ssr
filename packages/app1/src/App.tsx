6e0c9fe45cbb6d520c0ea7f5bb0b6845577612ae
devendra prasad <devendraprasad1984@gmail.com>
2022-03-04 19:02:41 +0530

    updates

--- packages/app1/src/App.tsx
+++ packages/app1/src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Button} from "@dp/app2";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0)
+    return (
+        <div className="App">
+            <h2>count: {count}</h2>
+            <Button text='click me' onClick={() => setCount(p => ++p)}/>
+        </div>
+    );
 }
 
 export default App;

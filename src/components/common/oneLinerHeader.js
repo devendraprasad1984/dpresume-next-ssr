5faed0f6bb99b83ad028da03ffa7c98139ca0f35
devendra prasad <devendraprasad1984@gmail.com>
2022-06-11 16:43:48 +0530

    some fixes

--- src/components/common/oneLinerHeader.js
+++ src/components/common/oneLinerHeader.js
@@ -1,14 +1,14 @@
 import PropTypes from "prop-types";
-import React, {useEffect} from "react";
-import {consoleText} from "../../configs/config";
+import React from "react";
+// import {consoleText} from "../../configs/config";
 
 const OneLinerHeader = ({title}) => {
-    useEffect(() => {
-        consoleText([title])
-    }, [])
+    // useEffect(() => {
+    //     consoleText([title])
+    // }, [])
     return <div className='console-container'>
         <span id='text'></span>
-        <div className='console-underscore' id='console'></div>
+        <div className='console-underscore size30' id='console'>{title}</div>
     </div>
 };
 

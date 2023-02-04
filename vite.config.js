ed22513b19053f8e47dd6fb530ea4cd328eee392
dp <dp@>
2023-02-04 18:24:00 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import {defineConfig} from 'vite'
+import {defineConfig, splitVendorChunkPlugin} from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs/promises';
 import {resolve} from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-    plugins: [react()],
-    server:{
+    plugins: [react(), splitVendorChunkPlugin()],
+    server: {
         port: 4201
 
     },
     resolve: {
         extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.vue'],
-        alias:{
-            'src': resolve(__dirname,'src'),
+        alias: {
+            'src': resolve(__dirname, 'src'),
         }
     },
     esbuild: {
@@ -27,7 +27,7 @@ export default defineConfig(() => ({
                 {
                     name: "load-js-files-as-jsx",
                     setup(build) {
-                        build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
+                        build.onLoad({filter: /src\/.*\.js$/}, async (args) => ({
                             loader: "jsx",
                             contents: await fs.readFile(args.path, "utf8"),
                         }));
@@ -39,10 +39,15 @@ export default defineConfig(() => ({
                 '.ts': 'tsx'
             }
         }
+    },
+    //below avoids hash to be added in final production build
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/dpresume.js`,
+                chunkFileNames: `assets/dpresumex.js`,
+                assetFileNames: `assets/[name].[ext]`
+            }
+        }
     }
-    // server:{
-    //   hmr:{
-    //     overlay: false
-    //   }
-    // }
 }))
